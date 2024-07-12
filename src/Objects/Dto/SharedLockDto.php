a7290bd725a64050a8895f60eb2a2a8e109588be
yrmltyrxnwtzsjlfvz <fake@knks.xyz>
2024-07-12 14:16:22 -0500

    Remove password from SharedLockDto as it's now in SharedLock

--- src/Objects/Dto/SharedLockDto.php
+++ src/Objects/Dto/SharedLockDto.php
@@ -3,41 +3,12 @@
 namespace Fake\ChasterDtoBundle\Objects\Dto;
 
 use Fake\ChasterObjects\Objects\Lock\SharedLock;
-use SensitiveParameter;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class SharedLockDto extends SharedLock
 {
     #[Assert\NotBlank]
-    private ?string $password = null;
-
-    #[Assert\NotBlank]
     private ?string $photoId = null;
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(#[SensitiveParameter] ?string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Helper method that sets the password and requirePassword fields.
-     *
-     * @param string|null $password
-     *
-     * @return $this
-     */
-    public function setPasswordRequired(#[SensitiveParameter] ?string $password = null): static
-    {
-        return $this->setPassword($password)
-            ->setRequirePassword(!empty($password));
-    }
 
     public function getPhotoId(): ?string
     {
@@ -47,6 +18,7 @@ class SharedLockDto extends SharedLock
     public function setPhotoId(?string $photoId): static
     {
         $this->photoId = $photoId;
+
         return $this;
     }
 }
