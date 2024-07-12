d5a61e7908809344d083b36d09a68e115c3014ca
yrmltyrxnwtzsjlfvz <fake@knks.xyz>
2024-07-12 10:28:52 -0500

    Add SharedLockDto

---
+++ src/Objects/Dto/SharedLockDto.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fake\ChasterDtoBundle\Objects\Dto;
+
+use Fake\ChasterObjects\Objects\Lock\SharedLock;
+use SensitiveParameter;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SharedLockDto extends SharedLock
+{
+    #[Assert\NotBlank]
+    private ?string $password = null;
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(#[SensitiveParameter] ?string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Helper method that sets the password and requirePassword fields.
+     *
+     * @param string|null $password
+     *
+     * @return $this
+     */
+    public function setPasswordRequired(#[SensitiveParameter] ?string $password = null): static
+    {
+        return $this->setPassword($password)
+            ->setRequirePassword(!empty($password));
+    }
+}
